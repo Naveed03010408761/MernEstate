362e3b6286c176f00093403735099a10fcf8b23c
Naveed03010408761 <akmal030104087@gmail.com>
2025-02-08 11:29:47 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-slate-500'>App</div>
+  )
+}
+
+export default App
