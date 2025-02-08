b12324292196d4eda420568d731481ca6ef14779
Naveed03010408761 <akmal030104087@gmail.com>
2025-02-08 11:51:36 +0500

    create pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'; 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+
+import './App.css';
 
 function App() {
   return (
-    <div className='bg-slate-500'>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
